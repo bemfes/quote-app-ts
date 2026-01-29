c63b98379e059aa864c71efee638fa1c9b4280c1
Anastasiia Ustin <2anastasiia@mail.ru>
2026-01-29 18:34:26 +0200

    added ErrorBoundary component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ErrorBoundary } from 'react-error-boundary'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ErrorBoundary fallback={<p>Sorry, it looks like an unexpected error has occurred.</p>}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ErrorBoundary>,
 )
